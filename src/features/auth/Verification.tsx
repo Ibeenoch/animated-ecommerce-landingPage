c7f024dc699043a973b23d7721029989ea2ac6bd
Mr Api <94024099+Ibeenoch@users.noreply.github.com>
2024-07-08 12:53:30 +0100

    Update Verification.tsx

--- src/features/auth/Verification.tsx
+++ src/features/auth/Verification.tsx
@@ -52,7 +52,7 @@ const Verification = () => {
       ) : (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Link to="/product">
+            <Link to="/">
               <div
                 style={{
                   width: "40px",
