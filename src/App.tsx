bb174c3010f67cd60ae99ebf6b0cbe76b0d194f0
Mr Api <94024099+Ibeenoch@users.noreply.github.com>
2024-07-08 12:43:35 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,6 @@ import ProductReviewFormPage from "./pages/ProductReviewFormPage";
 import Logout from "./features/auth/Logout";
 import PageNotFound from "./pages/PageNotFound";
 import { Toaster } from "react-hot-toast"
-import Starter from "./features/doc/Starter";
-import StarterOne from "./features/doc/StarterOne";
-import StarterTwo from "./features/doc/StarterTwo";
-import StarterThree from "./features/doc/StarterThree";
-import StarterFour from "./features/doc/StarterFour";
 
 
 const router = createBrowserRouter(
