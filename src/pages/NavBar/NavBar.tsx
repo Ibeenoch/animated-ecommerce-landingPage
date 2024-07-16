582c439233eb99f02595497edac0ea9cf3ac1687
Mr Api <94024099+Ibeenoch@users.noreply.github.com>
2024-07-16 20:20:53 +0100

    Update NavBar.tsx

--- src/pages/NavBar/NavBar.tsx
+++ src/pages/NavBar/NavBar.tsx
@@ -289,7 +289,7 @@ const NavBar: React.FC<Child> = ({ children, isOpen }) => {
                   </div>
                   <div className="-mr-2 flex md:hidden">
                     {/* Mobile menu button */}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-800 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Disclosure.Button className="relative inline-flex items-center justify-center bg-white p-2 text-gray-800 hover:text-gray-900 focus:outline-none focus:ring-0 focus:ring-white focus:ring-offset-0">
                       <span className="absolute -inset-0.5" />
                       <span className="sr-only">Open main menu</span>
                       {open ? (
