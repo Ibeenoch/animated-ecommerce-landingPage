d42373062cb2f2274095f8842873abb40fa9d950
Mr Api <94024099+Ibeenoch@users.noreply.github.com>
2024-07-16 19:55:32 +0100

    Update Loading.tsx

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -25,8 +25,6 @@ const [count, setCount] = useState<number>(45)
     </>
   ) : (
   <>
-  <p style={{ color: 'black', textAlign: 'center'}}> <strong>Please be patience, while we fetch the content</strong> </p>
-  <p style={{ color: 'black', textAlign: 'center'}}> <strong>this may take a while...</strong></p>
 <svg xmlns="http://www.w3.org/2000/svg" style={{ width: '150px', height: '150px' }} viewBox="0 0 200 200"><linearGradient id="a12"><stop offset="0" stop-color="#E50202" stop-opacity="0"></stop><stop offset="1" stop-color="#E50202"></stop></linearGradient><circle fill="none" stroke="url(#a12)" stroke-width="11" stroke-linecap="round" stroke-dasharray="0 44 0 44 0 44 0 44 0 360" cx="100" cy="100" r="70" transform-origin="center"><animateTransform type="rotate" attributeName="transform" calcMode="discrete" dur="2" values="360;324;288;252;216;180;144;108;72;36" repeatCount="indefinite"></animateTransform></circle></svg>    
 
   </>)}
