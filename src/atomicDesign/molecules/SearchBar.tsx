6e8d485211e1d55aefad371204ee50fb0136efa7
ibeenoch <fredenoch1@gmail.com>
2024-09-22 20:15:19 +0100

    make heart icon change icon when clicked

--- src/atomicDesign/molecules/SearchBar.tsx
+++ src/atomicDesign/molecules/SearchBar.tsx
@@ -21,7 +21,8 @@ const SearchBar = () => {
       <form className='flex items-center border-b border-b-[#8d8d8d] sm:border-b-[#000]'>
         <SearchBarLogo width={35} height={35} />
         <label className='hidden' htmlFor="search-for">Search for</label>
-        <input type="search" onChange={handleSearch} name="search-bar" id="search-bar" placeholder='type to search' className='text-[#8d8d8d] w-full bg-transparent border-0 focus:ring-0' />
+        {/* to hide border when the input field is clicked focus:outline-none */}
+        <input type="search" onChange={handleSearch} name="search-bar" id="search-bar" placeholder='type to search' className='text-[#8d8d8d] w-full bg-transparent border-0 focus:ring-0 focus:outline-none' />
       </form>
     </div>
   )
