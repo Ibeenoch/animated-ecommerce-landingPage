caa57abdd7a0d5527bd01daa41334b33a30169b2
ibeenoch <fredenoch1@gmail.com>
2024-09-22 19:27:09 +0100

    make heart icon change icon when clicked

--- src/atomicDesign/molecules/ShoppingIcons.tsx
+++ src/atomicDesign/molecules/ShoppingIcons.tsx
@@ -9,12 +9,12 @@ const ShoppingIcons = () => {
             <User className='w-6 h-6 sm:w-[40px] sm:h-[40px]' aria-hidden={true} />
         </span>
         <span className='cursor-pointer relative'>
-            <Heart className='w-4 h-4 sm:w-[30px] sm:h-[30px]' aria-hidden={true}  />
+            <Heart className='w-4 h-4 sm:w-[30px] sm:h-[30px]' stroke='gray' aria-hidden={true}  />
         </span>
         <span className='cursor-pointer relative'>
             <Cart className='w-4 h-4 sm:w-[30px] sm:h-[30px]' aria-hidden={true}  />
-            <span className='absolute bg-[#cad0c2] bottom-3 left-4 flex justify-center items-center px-2 py-[2.5px] rounded-full'>
-              <p className='text-center text-white text-[9px]'>0</p>
+            <span className='absolute bg-[#cad0c2] sm:bottom-3 bottom-1 sm:left-4 left-3 flex justify-center items-center sm:px-2 px-1 sm:py-[2.5px] py-[1.7px] rounded-full'>
+              <p className='text-center text-white sm:text-[9px] text-[6.5px]'>0</p>
             </span>
         </span>      
     </div>
